1caed6cf776bad2137126cc4c0dd67d89eebe199
tealicious <carterdanielr@gmail.com>
2019-03-07 17:39:41 -0500

    verse method added to lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,2 +1,7 @@
-class Lyrics {}
+class Lyrics {
+  verse(num) {
+    return `${num} bottles of beer on the wall, ${num} bottles of beer.
+Take one down and pass it around, ${num - 1} bottles of beer on the wall.`;
+  }
+}
 module.exports = Lyrics;
